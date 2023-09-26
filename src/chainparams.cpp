c3f9929a14b269aa303cb8868c696fa417594c96
Brown Brownie <browniecoinsdev@gmail.com>
2023-09-26 01:19:55 -0700

    update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,7 +94,7 @@ public:
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000200020");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        //consensus.defaultAssumeValid = uint256S("Oxe0feb9e314a113b1a70e8c60a502af65b7ff925f23d8595ec3fa5f53c82ce3f7"); // 0
+        consensus.defaultAssumeValid = uint256S("0x29913fd23951448eabdb964253889c65003784ffd9bbad564019e9939b0d2bf5"); // 0
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -115,7 +115,7 @@ public:
         std::cout << "Genesis Hash: " << consensus.hashGenesisBlock.ToString() << std::endl;
         std::cout << "MerkleRoot Hash: " << genesis.hashMerkleRoot.ToString() << std::endl;
 
-        assert(consensus.hashGenesisBlock == uint256S("0x3433a6a517adc9949d9684cdb569159fe9a2b734168ce11ef6b6b28055a6a815"));
+        assert(consensus.hashGenesisBlock == uint256S("0x29913fd23951448eabdb964253889c65003784ffd9bbad564019e9939b0d2bf5"));
         assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
@@ -144,7 +144,7 @@ public:
 
         checkpointData = {
             {
-                {  0, uint256S("0xe0feb9e314a113b1a70e8c60a502af65b7ff925f23d8595ec3fa5f53c82ce3f7")},
+                {  0, uint256S("0x29913fd23951448eabdb964253889c65003784ffd9bbad564019e9939b0d2bf5")},
             }
         };
 
