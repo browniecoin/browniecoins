8e8e67dd01d42d5d63274d6f5a076279837132da
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-14 11:35:01 -0700

    update

--- mine.sh
+++ mine.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if a wallet address argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <wallet_address>"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <wallet_address> <blocks>"
   exit 1
 fi
 
 wallet_address="$1"
-
+blocks="$2"
 #./src/brownied -printtoconsole -conf=/browniecoins/brownie.conf &
 #./src/brownied -conf=/browniecoins/brownie.conf
 nohup ./src/brownied -conf=/browniecoins/brownie.conf > brownied.log 2>&1 &
@@ -17,6 +17,6 @@ sleep 30
 while true
 do
   echo "Running Brownie Coin Miner"
-  ./src/brownie-cli -rpcport=8332 -rpcuser=brownie -rpcpassword=changeme generatetoaddress 1 "$wallet_address"
+  ./src/brownie-cli -rpcport=8332 -rpcuser=brownie -rpcpassword=changeme generatetoaddress "$blocks" "$wallet_address"
 #  sleep 1
 done
